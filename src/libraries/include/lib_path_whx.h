02fea333e128d4ab36eb7386222fd7562b2154de
SeamusWong <46064981+SeamusWong@users.noreply.github.com>
2023-07-11 22:23:32 +0800

    For ros noetic

--- src/libraries/include/lib_path_whx.h
+++ src/libraries/include/lib_path_whx.h
@@ -28,7 +28,6 @@ namespace LIB_PATH_WHX
         PathDataWHX &Init(void);
         PathDataWHX &AddPointFromMatrix4f(Eigen::Matrix4f m_mat,ros::Time m_time = ros::Time::now());
         PathDataWHX &AddPointFromMatrix4fForPose(Eigen::Matrix4f m_mat_last, Eigen::Matrix4f m_mat_this, CalibDataWHX m_calib, ros::Time m_time = ros::Time::now());
-        PathDataWHX &FAPP(Eigen::Matrix4f m_mat,ros::Time m_time = ros::Time::now());
         void PublishToRviz(ros::Publisher &m_publisher, const std::string m_frame_id = "odom");
         void PublishAllPoseStamped(ros::Publisher &m_publisher, const std::string m_frame_id = "odom");
         nav_msgs::PathConstPtr CallbackPath();
